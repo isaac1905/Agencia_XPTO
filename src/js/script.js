51b962ee6c210cc18cc47b34cbb7a28c48a3cddb
Isaac Emanuel <63521233+isaac1905@users.noreply.github.com>
2022-01-16 11:28:03 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ const setaAvancar = document.getElementById('btn-avancar');
 const setaVoltar = document.getElementById('btn-voltar');
 let imagemAtual = 0;
 
-
+//Funçao para trocar a imagem
 function Trocar() {
     imagensPainel.forEach(imagem => {
         imagem.classList.remove('mostrar');
@@ -13,32 +13,33 @@ function Trocar() {
 }
 
 
-setInterval(() => {
+// Funçao avançar o índice de imagemAtual e chamar a funçao de troca da imagem
+function avancar() {
     if (imagemAtual === imagensPainel.length - 1) {
         imagemAtual = 0;
     } else {
         imagemAtual++;
     }
     Trocar();
-}, 5000)
+}
 
 
-// Evento de click para avançar a imagem do botão avançar
-setaAvancar.addEventListener('click', () => {
-    if (imagemAtual === imagensPainel.length - 1) {
-        imagemAtual = 0;
-    } else {
-        imagemAtual++;
-    }
-    Trocar();
-})
-
-// Evento de click paraavançar a imagem do botão voltar
-setaVoltar.addEventListener('click', function() {
+// Funçao para voltar a imagem
+function voltar() {
     if (imagemAtual == 0) {
         imagemAtual = imagensPainel.length - 1
     } else {
         imagemAtual--
     }
     Trocar();
-})
+}
+
+
+// Avança a imagem a cada 5 segundos
+setInterval(avancar, 5000)
+
+// Evento de click para avançar a imagem do botão avançar
+setaAvancar.addEventListener('click', avancar)
+
+// Evento de click paraavançar a imagem do botão voltar
+setaVoltar.addEventListener('click', voltar)
